9295f0b2e859c783ff7571ad25e3a1d7ae35a1b0
Nuraaab <ahmednuru215@gmail.com>
2025-03-19 22:04:24 +0300

    update

--- app/Models/OfflinePayment.php
+++ app/Models/OfflinePayment.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OfflinePayment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'instruction', 'status'];
 }
--- app/Models/OnlinePayment.php
+++ app/Models/OnlinePayment.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OnlinePayment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'information', 'status'];
 }
